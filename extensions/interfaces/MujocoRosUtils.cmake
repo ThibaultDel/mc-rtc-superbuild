4b5ea0da581d5a7956bc68b6acbfc49131a7a412
ThibaultDel <delooeckerthibault@gmail.com>
2026-07-27 14:08:04 +0900

    mc_mujoco/mujocoRosUtils deleted

--- extensions/interfaces/MujocoRosUtils.cmake
+++
@@ -1,6 +0,0 @@
-AddProject(MujocoRosUtils
-        GITHUB ThibaultDel/MujocoRosUtils
-        GIT_TAG origin/main
-        CMAKE_ARGS -DMUJOCO_ROOT_DIR="$ENV{HOME}/.mujoco/mujoco-3.3.6"
-        DEPENDS mc_mujoco
-)
