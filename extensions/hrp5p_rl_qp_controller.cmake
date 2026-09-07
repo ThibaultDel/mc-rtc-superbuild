cba8f5752ba7d593aa188c7db80da4fc458d3d39
ThibaultDel <delooeckerthibault@gmail.com>
2026-09-07 11:25:30 +0900

    solved path/multiple files/non necessary nuggy files issue

--- extensions/hrp5p_rl_qp_controller.cmake
+++
@@ -1,5 +0,0 @@
-AddProject(hrp5p_rl_qp_controller
-  GITHUB_PRIVATE bastien-muraccioli/hrp5p_rl_qp_controller
-  GIT_TAG origin/main
-  DEPENDS mc_rtc
-)
--- extensions/local.cmake
+++ extensions/local.cmake
@@ -1,7 +1,7 @@
 set(EXTENSIONS_DIR ${CMAKE_CURRENT_LIST_DIR})
 include(${EXTENSIONS_DIR}/interfaces/mujoco.cmake)
 include(${EXTENSIONS_DIR}/interfaces/MujocoRosUtils.cmake)
-include(${EXTENSIONS_DIR}/controllers/hammeringtasknew.cmake)
+include(${EXTENSIONS_DIR}/hammeringtasknew.cmake)
 
 include(${EXTENSIONS_DIR}/mc_external_forces_observer.cmake)
 
